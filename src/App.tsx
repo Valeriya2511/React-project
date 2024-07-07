4ab8d6459b610f0325ee7e463bde7e2fc67e3347
Valeriya2511 <finsternis.ru@mail.ru>
2024-07-07 19:04:03 +0600

    feat: add a function for saving a request to local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
-import React, { useState } from 'react';
+import { ChangeEvent, Component } from 'react';
 import './App.css';
+import { SearchComponentState } from './interface/SearchComponent.interfaces';
 
-function App() {
-  const [count, setCount] = useState(0);
+class App extends Component<object, SearchComponentState> {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      searchQuery: '',
+    };
+  }
 
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+  componentDidMount() {
+    const savedQuery = localStorage.getItem('searchQuery');
+    if (savedQuery) {
+      this.setState({ searchQuery: savedQuery });
+    }
+  }
+
+  handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    this.setState({ searchQuery: value });
+  };
+
+  handleSearch = () => {
+    localStorage.setItem('searchQuery', this.state.searchQuery);
+  };
+  render() {
+    return (
+      <div className="app">
+        <div className="upper-section">
+          <input
+            type="text"
+            placeholder="Enter your search term..."
+            value={this.state.searchQuery}
+            onChange={this.handleInputChange}
+          />
+          <button onClick={this.handleSearch}>Search</button>
+        </div>
+        <div className="lower-section">
+          <h2>Нижняя секция</h2>
+          <p>Содержимое нижней секции...</p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/interface/SearchComponent.interfaces.ts
@@ -0,0 +1,3 @@
+export interface SearchComponentState {
+  searchQuery: string;
+}
