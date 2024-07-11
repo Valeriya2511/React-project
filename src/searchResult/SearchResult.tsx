0c790508d167be61848f433b7b2fab0489e286b6
Valeriya2511 <finsternis.ru@mail.ru>
2024-07-11 23:35:18 +0600

    feat: components in searchesult file changed to functional components

--- src/searchResult/SearchResult.tsx
+++ src/searchResult/SearchResult.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 interface SearchResult {
   name: string;
   url: string;
@@ -9,23 +7,38 @@ interface SearchResultsProps {
   results: SearchResult[];
 }
 
-class SearchResults extends React.Component<SearchResultsProps> {
-  render() {
-    const { results } = this.props;
-
-    return (
-      <div>
-        <h2>Search Results:</h2>
-        <ul>
-          {results.map((result, index) => (
-            <li key={index}>
-              <strong>{result.name}</strong>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+function SearchResults({ results }: SearchResultsProps) {
+  return (
+    <div>
+      <h2>Search Results:</h2>
+      <ul>
+        {results.map((result, index) => (
+          <li key={index}>
+            <strong>{result.name}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+// class SearchResults extends React.Component<SearchResultsProps> {
+//   render() {
+//     const { results } = this.props;
+
+//     return (
+//       <div>
+//         <h2>Search Results:</h2>
+//         <ul>
+//           {results.map((result, index) => (
+//             <li key={index}>
+//               <strong>{result.name}</strong>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
 
 export default SearchResults;
