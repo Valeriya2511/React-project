ca97566f3dc29d68de30bc13f5e47aa83195c242
Valeriya2511 <finsternis.ru@mail.ru>
2024-08-11 21:29:00 +0600

    feat: add components AboutPokemon and SearchResult

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import SearchBar from "@/components/searchBar";
+import SearchBar from "@/components/SearchBar";
+import SearchResults from "@/components/SearchResult";
 import { Advent_Pro } from "next/font/google";
 
 const inter = Advent_Pro({ subsets: ["latin"], weight: ["400", "700"] });
@@ -7,9 +8,15 @@ export default function Home() {
   const cons = () => {
     console.log('Hello');
   }
+
+  const rusult = {
+    name: 'pokemon',
+    url: 'https://pokeapi.co/api/v2/pokemon/ditto',
+  }
   return (
   <main className={`main ${inter.className}`}>
     <SearchBar lastSearchQuery="" onSearch={cons}></SearchBar>
+    <SearchResults results={[rusult]}></SearchResults>
   </main>
   )
 }
