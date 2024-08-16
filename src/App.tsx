eb6c54e50afd54222ec57be14b88d625612823f8
Valeriya2511 <finsternis.ru@mail.ru>
2024-08-17 00:25:08 +0600

    feat: add 3 routes (main, Uncontrolled Form and React Hook Form)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import UncontrolledForm from './components/UncontrolledForm'
+import ReactHookForm from './components/ReactHookForm'
+import Main from './components/Main'
 
-const App: React.FC = () => {
-  return <Router></Router>
+function App() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Main</Link>
+        <Link to="/uncontrolled-form">Uncontrolled Form</Link>
+        <Link to="/react-hook-form">React Hook Form</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/uncontrolled-form" element={<UncontrolledForm />}></Route>
+        <Route path="/react-hook-form" element={<ReactHookForm />}></Route>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
