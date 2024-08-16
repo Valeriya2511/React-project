ac8022ceac2ba48630df9aa8cbddd44d3a7696c7
Valeriya2511 <finsternis.ru@mail.ru>
2024-08-17 00:30:59 +0600

    feat: add Main component

---
+++ src/components/Main.tsx
@@ -0,0 +1,13 @@
+function Main() {
+  return (
+    <div>
+      <h1>Main page</h1>
+      <div>
+        <a href="/uncontrolled-form">Go to Uncontrolled Form</a>
+        <a href="/hook-form"> Go to React Hook Form</a>
+      </div>
+    </div>
+  )
+}
+
+export default Main
